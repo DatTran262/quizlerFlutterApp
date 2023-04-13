6d160f86acf3043549447a8e919ec769285aa48d
ĐạtTran262 <trandat262075@gmial.com>
2023-04-13 09:49:53 +0700

    Complete app

--- quizzler/lib/main.dart
+++ quizzler/lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:quizzler/quiz_brain.dart';
 import 'question.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+
+quizBrain QuizBrain = quizBrain();
 
 void main() => runApp(Quizzler());
 
@@ -9,7 +12,7 @@ class Quizzler extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.grey.shade900,
+        backgroundColor: Colors.blueAccent.shade200,
         body: SafeArea(
           child: Padding(
             padding: EdgeInsets.symmetric(horizontal: 10.0),
@@ -30,9 +33,39 @@ class _QuizPageState extends State<QuizPage> {
 
   List<Widget> scoreKeeper = [];
 
-  quizBrain QuizBrain = new quizBrain();
+  void checkAnswer(userPickAnswer) {
+    bool correctAnswer = QuizBrain.getQuestionAnswer();
+    if(userPickAnswer==correctAnswer){
+      scoreKeeper.add(
+          Icon(
+            Icons.check,
+            color: Colors.green,
+          )
+      );
+    } else {
+      scoreKeeper.add(
+          Icon(
+            Icons.close,
+            color: Colors.red,
+          )
+      );
+    }
+    setState(() {
+      QuizBrain.nextQuestion();
+    });
+  }
 
-  int questionNumber=0;
+  void checkFinished() {
+    if(QuizBrain.isFinished()==true){
+      Alert(
+        context: context,
+        title: 'Finished!',
+        desc: 'You\'ve reached the end of the quiz.',
+      ).show();
+      QuizBrain.reset();
+      scoreKeeper = [];
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -46,7 +79,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                QuizBrain.questionBank[questionNumber].questionText,
+                QuizBrain.getQuestionText(),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -73,25 +106,8 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked true.
-                bool correctAnswer = QuizBrain.questionBank[questionNumber].questionAnswer;
-                if(correctAnswer==true){
-                  scoreKeeper.add(
-                      Icon(
-                        Icons.check,
-                        color: Colors.green,
-                      )
-                  );
-                } else {
-                  scoreKeeper.add(
-                      Icon(
-                        Icons.close,
-                        color: Colors.red,
-                      )
-                  );
-                }
-                setState(() {
-                  questionNumber++;
-                });
+                checkAnswer(true);
+                checkFinished();
               },
             ),
           ),
@@ -113,25 +129,8 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked false.
-                bool correctAnswer = QuizBrain.questionBank[questionNumber].questionAnswer;
-                if(correctAnswer==true){
-                  scoreKeeper.add(
-                    Icon(
-                      Icons.close,
-                      color: Colors.red,
-                    )
-                  );
-                } else {
-                  scoreKeeper.add(
-                    Icon(
-                      Icons.check,
-                      color: Colors.green,
-                    )
-                  );
-                }
-                setState(() {
-                  questionNumber++;
-                });
+                checkAnswer(false);
+                checkFinished();
               },
             ),
           ),
--- quizzler/lib/quiz_brain.dart
+++ quizzler/lib/quiz_brain.dart
@@ -1,31 +1,66 @@
+import 'package:flutter/material.dart';
 import 'question.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
 
 class quizBrain {
-  List<Question> questionBank = [
-    Question(q:'Some cats are actually allergic to humans', a:true),
-    Question(q:'You can lead a cow down stairs but not up stairs.', a:false),
-    Question(q:'Approximately one quarter of human bones are in the feet.', a:true),
-    Question(q:'A slug\'s blood is green.', a:true),
-    Question(q:'Buzz Aldrin\'s mother\'s maiden name was \"Moon\".', a:true),
-    Question(q:'It is illegal to pee in the Ocean in Portugal.', a:true),
+
+  int _questionNumber=0;
+
+  List<Question> _questionBank = [
+    Question('Some cats are actually allergic to humans', true),
+    Question('You can lead a cow down stairs but not up stairs.', false),
+    Question('Approximately one quarter of human bones are in the feet.', true),
+    Question('A slug\'s blood is green.', true),
+    Question('Buzz Aldrin\'s mother\'s maiden name was \"Moon\".', true),
+    Question('It is illegal to pee in the Ocean in Portugal.', true),
     Question(
-        q:'No piece of square dry paper can be folded in half more than 7 times.',
-        a:false),
+        'No piece of square dry paper can be folded in half more than 7 times.',
+        false),
     Question(
-        q:'In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.',
-        a:true),
+        'In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.',
+        true),
     Question(
-        q:'The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.',
-        a:false),
+        'The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.',
+        false),
     Question(
-        q:'The total surface area of two human lungs is approximately 70 square metres.',
-        a:true),
-    Question(q:'Google was originally called \"Backrub\".', a:true),
+        'The total surface area of two human lungs is approximately 70 square metres.',
+        true),
+    Question('Google was originally called \"Backrub\".', true),
     Question(
-        q:'Chocolate affects a dog\'s heart and nervous system; a few ounces are enough to kill a small dog.',
-        a:true),
+        'Chocolate affects a dog\'s heart and nervous system; a few ounces are enough to kill a small dog.',
+        true),
     Question(
-        q:'In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.',
-        a:true),
+        'In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.',
+        true),
   ];
+
+  void nextQuestion(){
+    if(_questionNumber < _questionBank.length-1) _questionNumber++;
+  }
+
+  String getQuestionText(){
+    return _questionBank[_questionNumber].questionText;
+  }
+
+  bool getQuestionAnswer(){
+    return _questionBank[_questionNumber].questionAnswer;
+  }
+
+  bool isFinished() {
+    if (_questionNumber >= _questionBank.length - 1) {
+      //TODO: Step 3 Part B - Use a print statement to check that isFinished is returning true when you are indeed at the end of the quiz and when a restart should happen.
+
+      print('Now returning true');
+      return true;
+
+    } else {
+      return false;
+    }
+  }
+
+  //TODO: Step 4 part B - Create a reset() method here that sets the questionNumber back to 0.
+  void reset() {
+    _questionNumber = 0;
+  }
+
 }
